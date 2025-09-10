79ab20af52779fe8fd312797ef2ce354faf39146
OticasVisiotech <gabrielmgb.nd@gmail.com>
2025-09-09 23:39:58 -0300

    Organized code from Statement

--- src/components/Statement.jsx
+++ src/components/Statement.jsx
@@ -1,4 +1,3 @@
-
 const Statement = () => (
   <div id="statement" className="basic-1">
     <div className="container">
@@ -6,7 +5,12 @@ const Statement = () => (
         <div className="col-lg-12">
           <div className="text-container">
             <h3>The framework is a solid foundation for websites</h3>
-            <p className="p-large">“I've designed this framwork based on my experience with building websites for many types of industries. What's great about Gemdev is that it can be used by anyone from absolute beginners to skilled web developers from around the world”</p>
+            <p className="p-large">
+              “I've designed this framwork based on my experience with building
+              websites for many types of industries. What's great about Gemdev
+              is that it can be used by anyone from absolute beginners to
+              skilled web developers from around the world”
+            </p>
             <img src="src/assets/statement.jpg" alt="alternative" />
             <h6>Donna Staple, Founder</h6>
           </div>
