a288a077a362d8bfa0c091fe15da825f3e3f6f35
OticasVisiotech <gabrielmgb.nd@gmail.com>
2025-09-17 18:04:10 -0300

    Organized code from Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 
 const Header = () => {
   return (
@@ -15,14 +15,21 @@ const Header = () => {
                 Gemdev is a free HTML template designed for your web products
               </h1>
               <p className="p-large">
-                Use Gemdev to create awesome websites for your products, services and ideas in a short amount of time
+                Use Gemdev to create awesome websites for your products,
+                services and ideas in a short amount of time
               </p>
-              <a className="btn-solid-lg page-scroll" href="#statement">Discover</a>
+              <a className="btn-solid-lg page-scroll" href="#statement">
+                Discover
+              </a>
             </div>
           </div>
           <div className="col-lg-6">
             <div className="image-container">
-              <img className="img-fluid" src="src/assets/header-gem.svg" alt="alternative" />
+              <img
+                className="img-fluid"
+                src="src/assets/header-gem.svg"
+                alt="alternative"
+              />
             </div>
           </div>
         </div>
